bb8a2e2dfe998746f83cd8f142d064ca8513776e
jsphweid <jsphweid@gmail.com>
2021-06-13 08:01:56 -0500

    end to end kub

---
+++ Dockerfile
@@ -0,0 +1,18 @@
+FROM python:3.8-slim-buster
+
+RUN apt-get update && \
+  apt-get install -y liblo-dev librtmidi-dev build-essential python-dev libasound2-dev
+
+WORKDIR /app
+COPY audio audio
+COPY fn fn 
+COPY graph_predict graph_predict
+COPY midi midi
+COPY python_grpc_api python_grpc_api
+COPY requirements.txt requirements.txt
+COPY run_python_grpc_service.py run_python_grpc_service.py
+COPY type_model.py type_model.py
+COPY timer.py timer.py 
+
+RUN pip install -r requirements.txt
+CMD ["python", "run_python_grpc_service.py"]
--- cdk/index.ts
+++ cdk/index.ts
@@ -1,5 +1,6 @@
 import * as s3 from "@aws-cdk/aws-s3";
 import * as cdk from "@aws-cdk/core";
+import * as iam from "@aws-cdk/aws-iam";
 
 interface FnStackProps extends cdk.StackProps {
   readonly installPackages?: string;
@@ -9,11 +10,20 @@ export class FnStack extends cdk.Stack {
   constructor(scope: cdk.App, id: string, props: FnStackProps) {
     super(scope, id, props);
 
-    new s3.Bucket(this, "Bucket", {
+    const fnObjectsBucket = new s3.Bucket(this, "Bucket", {
       // TODO: `bucketName` should be a prop or default/autogen
       bucketName: "fn-objects",
       blockPublicAccess: s3.BlockPublicAccess.BLOCK_ALL,
     });
+
+    fnObjectsBucket.addLifecycleRule({ expiration: cdk.Duration.days(1) });
+
+    // NOTE: access key must be made in the console manually
+    const fnObjectBucketUser = new iam.User(this, "FnObjectBucketUser", {
+      userName: "fn-object-bucket-user",
+    });
+
+    fnObjectsBucket.grantReadWrite(fnObjectBucketUser);
   }
 }
 
